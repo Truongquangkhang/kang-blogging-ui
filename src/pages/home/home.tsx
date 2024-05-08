eda0a1e929cad0c94a19b84a301dcc0c17dcce94
Truongquangkhang <truongqkhang@gmail.com>
2024-05-08 19:12:32 +0700

    Add routes

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,10 @@
+
+const Home = ()=>{
+    return (
+        <>
+        <h1>HELLO</h1>
+        </>
+    )
+}
+
+export default Home
--- src/pages/router.tsx
+++ src/pages/router.tsx
@@ -1,1 +1,22 @@
-import { Navigate, useLocation } from 'react-router-dom'
+import { createBrowserRouter } from 'react-router-dom'
+import Home from './home/home.tsx'
+import Login from './login/index.tsx';
+import NotFound from './errors/forbidden/index.tsx';
+
+
+const Router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+    },
+    {
+        path: "/login",
+        element: <Login />,
+    },
+    {
+        path: "*",
+        element: <NotFound />
+    }
+])
+
+export default Router
