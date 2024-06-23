70c5d5cf926f9eb67776d2d997177210e95faf64
Truongquangkhang <truongqkhang@gmail.com>
2024-06-23 10:01:38 +0700

    fix pipeline

--- src/pages/logout/index.tsx
+++ src/pages/logout/index.tsx
@@ -1,15 +1,13 @@
-import { useNavigate } from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
 import { useAppDispatch } from '../../hooks'
 import { logOut } from '../../redux/reducers/auth'
 import { removeUser } from '../../redux/reducers/user'
 
 const LogOut = () => {
   const dispatch = useAppDispatch()
-  const navigate = useNavigate()
   dispatch(logOut())
   dispatch(removeUser())
-
-  navigate('/login')
+  return <Navigate to="/" />
 }
 
 export default LogOut
